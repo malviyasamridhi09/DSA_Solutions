4d0fb2fa237e80cd7df5dc2f4ef5244db8395516
malviyasamridhi09 <malviyasamridhi0930@gmail.com>
2026-03-06 22:20:13 +0530

    MaxProduct subarray code

---
+++ MaxProductsubarray.java
@@ -0,0 +1,26 @@
+
+public class MaxProductsubarray {
+
+    public static void main(String[] args){
+
+        int[] nums = {2,3,-2,4};
+
+        int max=nums[0],min=nums[0],result=nums[0];
+
+        for(int i=1;i<nums.length;i++){
+
+            if(nums[i] < 0){
+                int temp = max;
+                max = min;
+                min = temp;
+            }
+
+            max = Math.max(nums[i],max*nums[i]);
+            min = Math.min(nums[i],min*nums[i]);
+
+            result = Math.max(result,max);
+        }
+
+        System.out.println(result);
+    }
+}
